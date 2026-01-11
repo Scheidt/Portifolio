5f0fdadcbb58503b577a24a97756e92e40582f3a
Scheidt <pedrohenriqueprazeres@hotmail.com>
2026-01-11 14:32:42 -0300

    Feat: Contact Page implementation

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import ContactPage from "@components/contact/contact";
+import Navbar from "@ui/nav-menu";
+
+const ProfileHeaderDark = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div>
+      <ContactPage />
+      </div>
+    </div>
+  )
+
+}
+
+export default ProfileHeaderDark;
